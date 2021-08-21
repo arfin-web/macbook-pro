0fec2b885cecec3664632afebb4295389f3e1fdd
arfin-web <arfinpriom12@gmail.com>
2021-08-21 21:37:36 +0600

    complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//set value for no amount add
 function setValue0(cost0) {
     const cost = document.getElementById(cost0);
     parseInt(cost.innerText);
@@ -5,6 +6,7 @@ function setValue0(cost0) {
     cost.innerText = 0;
 }
 
+//set value for amount add
 function increasevalue(cost1, amount) {
     const increaseCost = document.getElementById(cost1);
     parseInt(increaseCost.innerText);
@@ -12,6 +14,8 @@ function increasevalue(cost1, amount) {
     increaseCost.innerText = amount;
 }
 
+
+//update amount
 function increaseCost() {
     const memoryCost = document.getElementById('memory-cost');
     const memoryCostValue = parseInt(memoryCost.innerText);
@@ -28,8 +32,13 @@ function increaseCost() {
     const totalPrice = document.getElementById('total-price');
     parseInt(totalPrice.innerText);
     totalPrice.innerText = bestCostValue + memoryCostValue + storageCostValue + deliveryCostValue;
+
+    const total = document.getElementById('total');
+    total.innerText = totalPrice.innerText;
 }
 
+
+//add click event to buttons and calculate
 document.getElementById('memory0').addEventListener('click', function () {
     setValue0('memory-cost');
     const increaseAmount = increaseCost();
@@ -39,6 +48,8 @@ document.getElementById('memory0').addEventListener('click', function () {
     const totalPrice = document.getElementById('total-price');
     parseInt(totalPrice.innerText);
     totalPrice.innerText = increaseAmountValue - memoryCostValue;
+    const total = document.getElementById('total');
+    total.innerText = totalPrice.innerText;
 })
 
 document.getElementById('storage0').addEventListener('click', function () {
@@ -50,6 +61,8 @@ document.getElementById('storage0').addEventListener('click', function () {
     const totalPrice = document.getElementById('total-price');
     parseInt(totalPrice.innerText);
     totalPrice.innerText = increaseAmountValue - storageCostValue;
+    const total = document.getElementById('total');
+    total.innerText = totalPrice.innerText;
 })
 
 document.getElementById('delivery0').addEventListener('click', function () {
@@ -61,6 +74,8 @@ document.getElementById('delivery0').addEventListener('click', function () {
     const totalPrice = document.getElementById('total-price');
     parseInt(totalPrice.innerText);
     totalPrice.innerText = increaseAmountValue - deliveryCostValue;
+    const total = document.getElementById('total');
+    total.innerText = totalPrice.innerText;
 })
 
 document.getElementById('memory16').addEventListener('click', function () {
@@ -81,4 +96,21 @@ document.getElementById('storage1').addEventListener('click', function () {
 document.getElementById('delivery20').addEventListener('click', function () {
     increasevalue('delivery-cost', 20);
     increaseCost();
+})
+
+
+//promo code calculate
+document.getElementById('apply').addEventListener('click', function () {
+    const promoInput = document.getElementById('promo-input');
+    const totalPrice = document.getElementById('total-price');
+    const total = document.getElementById('total');
+    const totalValue = parseInt(total.innerText);
+    if (promoInput.value == 'stevekaku') {
+        const discount = 20 / 100;
+        const discountCount = totalValue * discount;
+
+        total.innerText = totalValue - discountCount;
+        totalPrice.innerText = total.innerText;
+    }
+    promoInput.value = '';
 })
